84ce6ffa58f72aee06cc244a1ee309e91576492a
Hoang Duc Dat <hddhoangducdat@gmail.com>
2019-11-19 15:45:25 +0700

    make it better haha

--- models/productModel.js
+++ models/productModel.js
@@ -21,4 +21,4 @@ const productSchema = new mongoose.Schema({
   metaKeywords: String
 });
 
-module.exports = mongoose.model("product", productSchema, "product");
+module.exports = mongoose.model("product", productSchema);
