879edfe66f1ab58e62344281c6d5c583d405604c
Hoang Duc Dat <hddhoangducdat@gmail.com>
2019-11-19 16:24:41 +0700

    make it better haha

--- routes/categories.js
+++ routes/categories.js
@@ -1,36 +1,36 @@
-var express = require("express");
-var _ = require("lodash");
+var express = require('express');
+var _ = require('lodash');
 var router = express.Router();
-var productModel = require("../models/productModel");
+var productModel = require('../models/productModel');
 
 /* GET home page. */
 
-router.get("/:type", async (req, res) => {
+router.get('/:type', async (req, res) => {
   let type;
   switch (req.params.type) {
-    case "fashion":
-      type = "Thời trang";
+    case 'fashion':
+      type = 'Thời trang';
       break;
-    case "sport":
-      type = "Thể thao";
+    case 'sport':
+      type = 'Thể thao';
       break;
-    case "electronic":
-      type = "Điện tử";
+    case 'electronic':
+      type = 'Điện tử';
       break;
-    case "lifestyle":
-      type = "Nhà cửa - đời sống";
+    case 'lifestyle':
+      type = 'Nhà cửa - đời sống';
       break;
-    case "toolkit":
-      type = "Phụ kiện - thiết bị số";
+    case 'toolkit':
+      type = 'Phụ kiện - thiết bị số';
       break;
-    case "transport":
-      type = "Phương tiện";
+    case 'transport':
+      type = 'Phương tiện';
       break;
-    case "book":
-      type = "Sách";
+    case 'book':
+      type = 'Sách';
       break;
-    case "food":
-      type = "Thực phẩm";
+    case 'food':
+      type = 'Thực phẩm';
       break;
     default:
       break;
@@ -39,104 +39,104 @@ router.get("/:type", async (req, res) => {
     const carts = await productModel.find({
       Category: type
     });
-    res.render("categories", {
-      title: "Black Hole",
+    res.render('categories', {
+      title: 'Black Hole',
       condition: false,
       carts
     });
   }
 });
 
-router.get("/sport", async (req, res) => {
-  const carts = await productModel.find();
+// router.get("/sport", async (req, res) => {
+//   const carts = await productModel.find();
 
-  res.render("categories", {
-    title: "Black Hole",
-    condition: false,
-    carts: carts.filter(cart => {
-      return cart.Category === "Thể thao";
-    })
-  });
-});
+//   res.render("categories", {
+//     title: "Black Hole",
+//     condition: false,
+//     carts: carts.filter(cart => {
+//       return cart.Category === "Thể thao";
+//     })
+//   });
+// });
 
-router.get("/fashion", async (req, res) => {
-  const carts = await productModel.find();
-  res.render("categories", {
-    title: "Black Hole",
-    condition: false,
-    carts: carts.filter(cart => {
-      return cart.Category === "Thời trang";
-    })
-  });
-});
+// router.get("/fashion", async (req, res) => {
+//   const carts = await productModel.find();
+//   res.render("categories", {
+//     title: "Black Hole",
+//     condition: false,
+//     carts: carts.filter(cart => {
+//       return cart.Category === "Thời trang";
+//     })
+//   });
+// });
 
-router.get("/electronic", async (req, res) => {
-  const carts = await productModel.find();
-  res.render("categories", {
-    title: "Black Hole",
-    condition: false,
-    carts: carts.filter(cart => {
-      return cart.Category === "Điện tử";
-    })
-  });
-});
+// router.get("/electronic", async (req, res) => {
+//   const carts = await productModel.find();
+//   res.render("categories", {
+//     title: "Black Hole",
+//     condition: false,
+//     carts: carts.filter(cart => {
+//       return cart.Category === "Điện tử";
+//     })
+//   });
+// });
 
-router.get("/transport", async (req, res) => {
-  const carts = await productModel.find();
+// router.get("/transport", async (req, res) => {
+//   const carts = await productModel.find();
 
-  res.render("categories", {
-    title: "Black Hole",
-    condition: false,
-    carts: carts.filter(cart => {
-      return cart.Category === "Phương tiện";
-    })
-  });
-});
+//   res.render("categories", {
+//     title: "Black Hole",
+//     condition: false,
+//     carts: carts.filter(cart => {
+//       return cart.Category === "Phương tiện";
+//     })
+//   });
+// });
 
-router.get("/book", async (req, res) => {
-  const carts = await productModel.find();
+// router.get("/book", async (req, res) => {
+//   const carts = await productModel.find();
 
-  res.render("categories", {
-    title: "Black Hole",
-    condition: false,
-    carts: carts.filter(cart => {
-      return cart.Category === "Sách";
-    })
-  });
-});
+//   res.render("categories", {
+//     title: "Black Hole",
+//     condition: false,
+//     carts: carts.filter(cart => {
+//       return cart.Category === "Sách";
+//     })
+//   });
+// });
 
-router.get("/toolkit", async (req, res) => {
-  const carts = await productModel.find();
+// router.get("/toolkit", async (req, res) => {
+//   const carts = await productModel.find();
 
-  res.render("categories", {
-    title: "Black Hole",
-    condition: false,
-    carts: carts.filter(cart => {
-      return cart.Category === "Phụ kiện - thiết bị số";
-    })
-  });
-});
+//   res.render("categories", {
+//     title: "Black Hole",
+//     condition: false,
+//     carts: carts.filter(cart => {
+//       return cart.Category === "Phụ kiện - thiết bị số";
+//     })
+//   });
+// });
 
-router.get("/food", async (req, res) => {
-  const carts = await productModel.find();
-  res.render("categories", {
-    title: "Black Hole",
-    condition: false,
-    carts: carts.filter(cart => {
-      return cart.Category === "Thực phẩm";
-    })
-  });
-});
+// router.get("/food", async (req, res) => {
+//   const carts = await productModel.find();
+//   res.render("categories", {
+//     title: "Black Hole",
+//     condition: false,
+//     carts: carts.filter(cart => {
+//       return cart.Category === "Thực phẩm";
+//     })
+//   });
+// });
 
-router.get("/lifestyle", async (req, res) => {
-  const carts = await productModel.find();
-  res.render("categories", {
-    title: "Black Hole",
-    condition: false,
-    carts: carts.filter(cart => {
-      return cart.Category === "Nhà cửa - đời sống";
-    })
-  });
-});
+// router.get("/lifestyle", async (req, res) => {
+//   const carts = await productModel.find();
+//   res.render("categories", {
+//     title: "Black Hole",
+//     condition: false,
+//     carts: carts.filter(cart => {
+//       return cart.Category === "Nhà cửa - đời sống";
+//     })
+//   });
+// });
 
 module.exports = router;
--- routes/homePage.js
+++ routes/homePage.js
@@ -1,9 +1,9 @@
-var express = require("express");
-var productModel = require("../models/productModel");
+var express = require('express');
+var productModel = require('../models/productModel');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const carts = await productModel.find();
   // const carts = [
   //   {
@@ -31,30 +31,30 @@ router.get("/", async (req, res) => {
   //   }
   // ];
   //res.send(carts);
-  res.render("homePage", {
-    title: "Black Hole",
+  res.render('homePage', {
+    title: 'Black Hole',
     condition: false,
     carousel: [
       {
         cartId: 1,
-        picture: "https://cdn.mos.cms.futurecdn.net/HvjfsxzQHCZxpUYTVgyBDM.jpg",
-        caption: "MacBook Air (2019)",
+        picture: 'https://cdn.mos.cms.futurecdn.net/HvjfsxzQHCZxpUYTVgyBDM.jpg',
+        caption: 'MacBook Air (2019)',
         condition: true,
         id: 0
       },
       {
         cartId: 5,
         picture:
-          "https://cdn.motor1.com/images/mgl/MoVM6/s1/lamborghini-sc18-alston.jpg",
-        caption: "Lamborghini Aventador Successor Allegedly Arriving In 2020",
+          'https://cdn.motor1.com/images/mgl/MoVM6/s1/lamborghini-sc18-alston.jpg',
+        caption: 'Lamborghini Aventador Successor Allegedly Arriving In 2020',
         condition: false,
         id: 1
       },
       {
         cartId: 6,
         picture:
-          "https://tallypress.com/wp-content/uploads/2018/09/Best-Fiction-and-Nonfiction-Books-of-2017.jpg",
-        caption: "Tuyển tập những quyển sách hay",
+          'https://tallypress.com/wp-content/uploads/2018/09/Best-Fiction-and-Nonfiction-Books-of-2017.jpg',
+        caption: 'Tuyển tập những quyển sách hay',
         condition: false,
         id: 2
       }
